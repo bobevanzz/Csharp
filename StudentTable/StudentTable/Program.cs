b04f310b046a429b41ca441701a8351d91942c88
Bob Evans <bedrockballer@gmail.com>
2018-02-22 13:37:52 -0500

    Finished?

--- StudentTable/StudentTable/Program.cs
+++ StudentTable/StudentTable/Program.cs
@@ -12,20 +12,31 @@ namespace StudentTable
         static void Main(string[] args)
         {
             IntroDbContext db = new IntroDbContext();
-            //Student[] students = db.Students.ToArray();
-            Student[] students = db.Students.Where(Student => Student.GradWithHonors == true).OrderBy(student=> student.GradYear).ToArray();
-            foreach (Student student in students)
-            {
-                string message = $"Name={student.Name}, Honor Grad={student.GradWithHonors}, Graduation year={student.GradYear}";
-                Console.WriteLine(message);
-            }
-            Console.ReadKey();
+            //Student[] students = db.Students.ToArray(); //Task 1
             //foreach (Student student in students)
             //{
-            //    string message = $"Name={student.Name}, Graduated with honors={student.GradWithHonors}, ";
+            //    string message = $"Id={student.Id}";
             //    Console.WriteLine(message);
             //}
             //Console.ReadKey();
+
+            //Student[] students = db.Students.ToArray();
+            //foreach (Student student in students) // Task 2
+            //{
+            //    string message = $"Name={student.Name}, ";
+            //    Console.WriteLine(message);
+            //}
+            //Console.ReadKey();
+
+            //Student[] students = db.Students.Where(Student => Student.GradWithHonors == true) //Task 3
+            //                                .OrderBy(student => student.GradYear).ToArray();
+            //foreach (Student student in students)
+            //{
+            //    string message = $"Name={student.Name}, Honor Grad={student.GradWithHonors}, Graduation year={student.GradYear}";
+            //    Console.WriteLine(message);
+            //}
+            //Console.ReadKey();
+
         }
     }
 }
