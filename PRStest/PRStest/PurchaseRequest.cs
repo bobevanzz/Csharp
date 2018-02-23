c2752089823d08642e25e8ad002523035fadab3c
Bob Evans <bedrockballer@gmail.com>
2018-02-22 23:43:05 -0500

    added all Classes, now to fill the data

--- PRStest/PRStest/PurchaseRequest.cs
+++ PRStest/PRStest/PurchaseRequest.cs
@@ -10,7 +10,6 @@ namespace PRStest
     public class PurchaseRequest
     {
         public int Id { get; set; }
-        public virtual User UserId { get; set; }
         public virtual User User { get; set; }
         [StringLength(100)]
         [Required]
@@ -18,26 +17,22 @@ namespace PRStest
         [StringLength(255)]
         [Required]
         public string Justification { get; set; }
-        [Required]
         public DateTime DateNeeded { get; set; }
         [StringLength(25)]
         [Required]
         public string DeliveryMode { get; set; }
-        [Required]
         public int StatusId { get; set; }
         [Required]
-        public double Total { get; set; }
-        [Required]
+        public decimal Total { get; set; }
         public DateTime SubmittedDate { get; set; }
         [Required]
         public bool Active { get; set; }
         [StringLength(100)]
-        [Required]
         public string ReasonForRejection { get; set; }
-        [Required]
+
         public DateTime DateCreated { get; set; }
-        [Required]
+
         public DateTime DateUpdated { get; set; }
-        
+
     }
 }
