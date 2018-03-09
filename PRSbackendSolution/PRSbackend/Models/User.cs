d3b6e247a71efb789d7f4bdc6fe07078cf8da239
Bob Evans <bedrockballer@gmail.com>
2018-03-09 15:45:00 -0500

    added controller to user

--- PRSbackendSolution/PRSbackend/Models/User.cs
+++ PRSbackendSolution/PRSbackend/Models/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,15 +10,31 @@ namespace PRSbackend.Models
     public class User
     {
         public int Id { get; set; }
+
+        [StringLength(30)] 
+        [Required] 
+        [Index(IsUnique = true)] 
         public string UserName { get; set; }
+        [StringLength(30)]
+        [Required]
         public string Password { get; set; }
+        [StringLength(30)]
+        [Required]
         public string FirstName { get; set; }
+        [StringLength(30)]
+        [Required]
         public string LastName { get; set; }
+        [StringLength(12)]
+        [Required]
         public string Phone { get; set; }
+        [StringLength(75)]
+        [Required]
         public string Email { get; set; }
-        public bool  IsReviewer { get; set; }
+        [Required]
+        public bool IsReviewer { get; set; }
+        [Required]
         public bool IsAdmin { get; set; }
-        public bool Active { get; set; }
+        public bool  Active { get; set; }
         public DateTime DateCreated { get; set; }
     }
 }
