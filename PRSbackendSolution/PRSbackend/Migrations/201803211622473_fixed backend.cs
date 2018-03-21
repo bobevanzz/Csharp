3e591b5819842f50c17d93612ec7e2d3f279571d
Bob Evans <bedrockballer@gmail.com>
2018-03-21 12:23:23 -0400

    migration

---
+++ PRSbackendSolution/PRSbackend/Migrations/201803211622473_fixed backend.cs
@@ -0,0 +1,16 @@
+namespace PRSbackend.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fixedbackend : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
